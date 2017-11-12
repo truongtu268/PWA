44b54fc8a3de0db795e0ad024f9be71fd904bfb2
truongtu <truongtu268@gmail.com>
2017-11-12 19:59:29 +0700

    update final workbox example
    final workbox

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,18 +1,72 @@
 importScripts('workbox-sw.prod.v2.1.1.js');
+importScripts('/src/js/idb.js')
+importScripts('/src/js/utility.js')
 
 const workboxSW = new self.WorkboxSW();
+
+workboxSW.router.registerRoute(/.*(?:googleapis|gstatic)\.com.*$/, workboxSW.strategies.staleWhileRevalidate({
+    cacheName: 'google-fonts',
+    cacheExpiration: {
+        maxEntries: 3,
+        maxAgeSeconds: 60 * 60 * 24 * 30
+    }
+}));
+
+workboxSW.router.registerRoute('https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css', workboxSW.strategies.staleWhileRevalidate({
+    cacheName: 'materials-css'
+}));
+
+workboxSW.router.registerRoute(/.*(?:firebasestorage\.googleapis|gstatic)\.com.*$/, workboxSW.strategies.staleWhileRevalidate({
+    cacheName: 'image-posts'
+}));
+
+workboxSW.router.registerRoute('https://pwagram-2f213.firebaseio.com/posts.json', function (args) {
+    return fetch(args.event.request).then(function (res) {
+        var cloneRes = res.clone();
+        clearAllData('posts').then(function () {
+            return cloneRes.json()
+        }).then(function (data) {
+            for (var key in data) {
+                writeData('posts', data[key])
+            }
+        });
+        return res;
+    });
+});
+
+workboxSW.router.registerRoute(function (routeData) {
+    return (routeData.event.request.headers.get('accept').includes('text/html'));
+}, function (args) {
+    return caches.match(args.event.request)
+        .then(function (response) {
+            if (response) {
+                return response;
+            } else {
+                return fetch(args.event.request)
+                    .then(function (res) {
+                        return caches.open('dynamic')
+                            .then(function (cache) {
+                                cache.put(args.event.request.url, res.clone());
+                                return res;
+                            })
+                    });
+            }
+        })
+        .catch(function (err) {
+            return caches.match('/offline.html').then(function (res) {
+                return res;
+            });
+        });
+});
+
 workboxSW.precache([
-  {
-    "url": "404.html",
-    "revision": "0a27a4163254fc8fce870c8cc3a3f94f"
-  },
   {
     "url": "favicon.ico",
     "revision": "2cab47d9e04d664d93c8d91aec59e812"
   },
   {
     "url": "index.html",
-    "revision": "fd48136656e4e3192e3a1e6214e6a965"
+    "revision": "c2310079215ad713e2f4424a9fa79c4e"
   },
   {
     "url": "manifest.json",
@@ -20,7 +74,7 @@ workboxSW.precache([
   },
   {
     "url": "offline.html",
-    "revision": "45352e71a80a5c75d25e226e7330871b"
+    "revision": "28f61f51ff8710887e4786b82b0ce29a"
   },
   {
     "url": "src/css/app.css",
@@ -33,46 +87,6 @@ workboxSW.precache([
   {
     "url": "src/css/help.css",
     "revision": "1c6d81b27c9d423bece9869b07a7bd73"
-  },
-  {
-    "url": "src/js/app.js",
-    "revision": "ccdb6a86e85a0b21d1bdc644e8df0170"
-  },
-  {
-    "url": "src/js/feed.js",
-    "revision": "7dcb70a99ed65cd8febac7a1c3bc4862"
-  },
-  {
-    "url": "src/js/fetch.js",
-    "revision": "6b82fbb55ae19be4935964ae8c338e92"
-  },
-  {
-    "url": "src/js/idb.js",
-    "revision": "017ced36d82bea1e08b08393361e354d"
-  },
-  {
-    "url": "src/js/material.min.js",
-    "revision": "713af0c6ce93dbbce2f00bf0a98d0541"
-  },
-  {
-    "url": "src/js/promise.js",
-    "revision": "10c2238dcd105eb23f703ee53067417f"
-  },
-  {
-    "url": "src/js/utility.js",
-    "revision": "c0cac39531b6eed677dd8cfddfa487cc"
-  },
-  {
-    "url": "sw-base.js",
-    "revision": "194e239ee431f58f3dbe3c549a74a329"
-  },
-  {
-    "url": "sw.js",
-    "revision": "b6f31558f54e98a71b8d68ba5eeb969a"
-  },
-  {
-    "url": "workbox-sw.prod.v2.1.1.js",
-    "revision": "2a5638f9e33d09efc487b96804a0aa11"
   },
   {
     "url": "src/images/main-image-lg.jpg",
@@ -89,5 +103,116 @@ workboxSW.precache([
   {
     "url": "src/images/sf-boat.jpg",
     "revision": "0f282d64b0fb306daf12050e812d6a19"
+  },
+  {
+    "url": "src/js/app.min.js",
+    "revision": "fa29c8cfd9bdeb85349debfd98bcc60e"
+  },
+  {
+    "url": "src/js/feed.min.js",
+    "revision": "07f65bf577f4b66470945f51116f7cd9"
+  },
+  {
+    "url": "src/js/fetch.min.js",
+    "revision": "a3d051e37ea10d8d5c7ae77aedaa87e9"
+  },
+  {
+    "url": "src/js/idb.min.js",
+    "revision": "b2ac265af44e1bf2c224c0283d5ccdfb"
+  },
+  {
+    "url": "src/js/material.min.js",
+    "revision": "713af0c6ce93dbbce2f00bf0a98d0541"
+  },
+  {
+    "url": "src/js/promise.min.js",
+    "revision": "9bbe6503bbed57e8d4992e0a174d3305"
+  },
+  {
+    "url": "src/js/utility.min.js",
+    "revision": "49a30a3b306d86ac4d16245197ae0e5d"
   }
 ]);
+
+self.addEventListener('sync', function (event) {
+    console.log('[Service Worker] Background syncing', event)
+    if (event.tag === 'sync-new-posts') {
+        console.log('[Service Worker] Syncing new Posts')
+        event.waitUntil(
+            readAllData('sync-posts').then(function (data) {
+                for(var dt of data){
+                    var postData = new FormData();
+                    postData.append('id',dt.id);
+                    postData.append('title',dt.title);
+                    postData.append('location',dt.location);
+                    postData.append('rawLocationLat',dt.rawLocation.lat);
+                    postData.append('rawLocationLng',dt.rawLocation.lng);
+                    postData.append('file',dt.picture,dt.id+'.png');
+                    fetch('https://us-central1-pwagram-2f213.cloudfunctions.net/storePostData',{
+                        method: 'POST',
+                        body: postData
+                    }).then(function (res) {
+                        console.log('Send data', res)
+                        if (res.ok) {
+                            deleteItemFromDatabase('sync-posts', dt.id);
+                        }
+                    }).catch(function (err) {
+                        console.log('Error while sending data', err)
+                    })
+                }
+            })
+        )
+    }
+});
+
+self.addEventListener('notificationclick', function (event) {
+    var notification = event.notification;
+    var action = event.action;
+    if (action === 'confirm'){
+        console.log('Confirm was chosen');
+        notification.close();
+    } else {
+        console.log(action);
+        event.waitUntil(
+            clients.matchAll().then(function (clis) {
+                var client = clis.find(function (c) {
+                    return c.visibilityState === 'visible'
+                });
+
+                if(client !== undefined){
+                    client.navigate(notification.data.url)
+                    client.focus()
+                } else {
+                    clients.openWindow(notification.data.url)
+                }
+                notification.close()
+            })
+        )
+        notification.close();
+    }
+})
+
+self.addEventListener('notificationclose', function (event) {
+    console.log('Notification was closed', event)
+})
+
+self.addEventListener('push',function (event) {
+    console.log('Push Notification received', event)
+    var data = {title: 'New!',content: 'Something new happened!',openUrl:'/'}
+    if (event.data) {
+        data = JSON.parse(event.data.text());
+    }
+
+    var options = {
+        body: data.content,
+        icon: '/src/images/icons/app-icon-96x96.png',
+        badge: '/src/images/icons/app-icon-96x96.png',
+        data: {
+            url: data.openUrl
+        }
+    };
+
+    event.waitUntil(
+        self.registration.showNotification(data.title, options)
+    )
+})
